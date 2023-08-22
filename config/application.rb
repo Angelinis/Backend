49b16ff90ec65b755c8f2a399fd47fc584b090f7
Angelo C <angmogollon@gmail.com>
2023-08-22 19:01:11 -0300

    Fix cros

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,7 @@ module Backend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: :any
-      end
-    end
+
 
 
   end
