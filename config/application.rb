e7c4895c614cbe2d8ea91307ebfb034ac3be5e71
Angelo C <angmogollon@gmail.com>
2023-08-22 02:24:33 -0300

    Fix CORS policy

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,14 @@ module Backend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    use Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: :any
-      end  
+      end
     end
 
-    
+
   end
 end
 
