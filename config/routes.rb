e5023eaa4817a5117bfff4079669296b146ef431
Angelo C <angmogollon@gmail.com>
2023-08-22 17:53:19 -0300

    Add post route to notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :notes, only: [:index, :show, :update, :destroy]
+  resources :notes, only: [:index, :show, :update, :destroy, :create]
   resources :users, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
