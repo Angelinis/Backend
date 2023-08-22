39ff687a00cc0d1d0dc11c31a0ceb4caa564a6d3
Angelo C <angmogollon@gmail.com>
2023-08-22 00:41:06 -0300

    Add controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,46 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: %i[show update destroy]
+  def index
+    @notes = Note.all
+    render json: @notes
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      render json: @note, status: :created
+    else
+      render json: @note.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note, status: :created
+    else
+      render json: @note.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @note
+  end
+
+  def destroy
+    @note.destroy
+    notes = Note.all
+    render json: notes, notice: "Note successfully destroyed", status: :see_other
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content, :user_id, :archived)
+  end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :notes, only: [:index, :show, :update, :destroy]
+  resources :users, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
